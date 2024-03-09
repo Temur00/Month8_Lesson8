87e03b64a4223e0f0333fbcd3b0e200bb426453b
Temur <shernazarovtemurcom001@gmail.com>
2024-03-09 18:12:21 +0500

    header is done

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,6 @@
 import Logo from "./logos/Logo.svg";
+import SearchIcon from "./icons/searchIcon.png";
+import Basket from "./icons/basket.png";
+import Heart from "./icons/heart.png";
 
-export { Logo };
+export { Logo, SearchIcon, Basket, Heart };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Logo } from "@/assets/images";
+import { Basket, Heart, Logo, SearchIcon } from "@/assets/images";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -18,8 +18,8 @@ const Header = () => {
         </div>
         <div></div>
       </div>
-      <div className="bg-white fixed left-0 right-0 h-[75px] py-4">
-        <div className="container mx-auto w-[112px] h-[31.75px] flex">
+      <div className="bg-white flex fixed left-0 right-0 h-[75px] top- py-4">
+        <div className="container mx-auto  h-[31.75px] flex items-center justify-between pt-2">
           <Image src={Logo} alt="Logo" />
           <button className="py-2 duration-200 px-3 h-[43px] w-52 rounded-lg flex  items-center justify-center gap-2 relative font-medium bg-yellow-500">
             <div className=" bg-black w-6 h-1 block -mt-4">
@@ -28,6 +28,31 @@ const Header = () => {
               <span className="block mt-1  h-1 bg-black"></span>
             </div>
             <span className="hidden lg:block w-32">Tovarlar katalogi</span>
+          </button>
+          <div>
+            <form className="flex items-center relative  bg-yellow-500 h-[42px] rounded-md">
+              <div className="w-64 md:w-72 lg:w-80 xl:w-96">
+                <input
+                  type="search"
+                  className="w-full ml-[3px] pl-4 text-sm flex items-center rounded-s-lg border-yellow-500 placeholder:text-gray-600 outline-none border-2  h-[36px]"
+                  placeholder="Tovarlarni izlash"
+                />
+              </div>
+              <button type="submit" className="p-4">
+                <Image src={SearchIcon} alt="searchicon" className="w-6 h-6" />
+              </button>
+            </form>
+          </div>
+          <div className="h-[43px] gap-1 items-center flex flex-col cursor-pointer">
+            <Image src={Basket} alt="basketIco" className="w-5 h-5" />
+            <p className="text-[12px]">Savat</p>
+          </div>
+          <div className="h-[43px] gap-1 items-center flex-col cursor-pointer hidden sm:flex">
+            <Image src={Heart} alt="basketIco" className="w-5 h-5" />
+            <p className="text-[12px]">Saralanganlar</p>
+          </div>
+          <button className="border-yellow-500 border-2 w-[63px] h-[42px] rounded-lg text-sm hover:bg-yellow-100 ease-in duration-300">
+            Kirish
           </button>
         </div>
       </div>
